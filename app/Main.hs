e2353f550f4e18bddd54bbdd50996ca759122627
Nikita Lebedev <eazy-f@yandex.ru>
2017-06-28 23:55:39 +0100

    Show pipe data using Brick GUI

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,115 @@ module Main where
 import Lib
 import Control.Monad
 import Control.Applicative
+import Control.Concurrent as Concurrent
 import Data.Maybe (fromMaybe)
 import Data.ByteString.Base16 (decode)
 import Data.ByteString.Char8 (pack, ByteString)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Time.Clock as T
 import Parser (evalParser, readByte, stop, ParserState)
+import qualified GHC.IO.Handle as H
+import qualified System.Environment as SysEnv
+import qualified System.Process as SysProc
+import qualified Brick.Main as BM
+import qualified Brick.Types as BT
+import qualified Brick.Widgets.Core as BC
+import qualified Brick.Widgets.Border as BWB
+import qualified Brick.Widgets.List as BWL
+import qualified Brick.Widgets.Center as BWC
+import qualified Brick.BChan as BChan
+import qualified Graphics.Vty as V
+import Brick.AttrMap (attrMap)
+import qualified Data.Vector as Vec
 
 type Parser = (Direction, ParserState WinDbgMsg ByteString)
 data WinDbgMsg = WinDbgMsgNull deriving Show
 
+data ChannelStats = ChannelStats { stats_in :: Integer,
+                                   stats_out :: Integer,
+                                   last_msg_time :: T.UTCTime}
+data Direction = Inbound | Outbound deriving Eq
+data ChannelMessage = ChannelMessage Direction ByteString deriving Show
+
+data ChannelArgs = ChannelArgs {original :: String, duplicate :: String}
+type SocatMsg = (Direction, String)
+
+instance Show Direction where
+  show Inbound = "in"
+  show Outbound = "out"
 
 main :: IO ()
-main = newStats >>= readAndReport newParsers
+main = do
+  args <- readArgs
+  channel <- prepareChannel args
+  displayActivity channel
+
+readArgs = do
+  [pipe] <- SysEnv.getArgs
+  return $ ChannelArgs {original = pipe, duplicate = duplicatePipeName pipe}
+
+duplicatePipeName pipeName = pipeName ++ ".dup"
+
+prepareChannel args = do
+  (_, _, Just hOut, _) <- SysProc.createProcess socat{SysProc.std_err = SysProc.CreatePipe}
+  chan <- BChan.newBChan 100
+  Concurrent.forkIO $ readPipe hOut chan
+  return chan
+  where
+    socatPipe = (++) "PIPE:"
+    [originalPipe, duplicatePipe] = map (socatPipe . ($ args)) [original, duplicate]
+    socat = SysProc.proc "socat" ["-x", originalPipe, duplicatePipe]
+
+readPipe pipe chan = forever $ readPipeMsg pipe >>= writeChanMsg chan
+
+readPipeMsg pipe = do
+  (direction, _) <- fmap parseMsg $ H.hGetLine pipe
+  fmap ((,) direction) $ H.hGetLine pipe
+
+
+parseMsg (direction:' ':payload) = (parseDirection direction, payload)
+
+parseDirection '>' = Inbound
+parseDirection '<' = Outbound
+
+writeChanMsg :: BChan.BChan SocatMsg -> SocatMsg -> IO ()
+writeChanMsg = BChan.writeBChan
+
+uiApp = BM.App {BM.appDraw = drawUI,
+                BM.appChooseCursor = BM.showFirstCursor,
+                BM.appHandleEvent = uiAppEvent,
+                BM.appStartEvent = return,
+                BM.appAttrMap = const $ attrMap V.defAttr []}
+
+drawUI list = [ui]
+  where
+    label = BC.str "potok"
+    box = BWB.borderWithLabel label $
+               BC.hLimit 25 $
+               BC.vLimit 15 $
+               BWL.renderList listDrawElement True list
+    ui = BWC.vCenter $ BC.vBox [BWC.hCenter box]
+
+listDrawElement _sel msg = BC.str $ show msg
+
+uiAppEvent msgs event =
+  case event of
+   BT.AppEvent msg ->
+     let first = 0
+     in BM.continue $ BWL.listInsert first msg msgs
+   BT.VtyEvent (V.EvKey (V.KChar 'q') []) ->
+     BM.halt msgs
+   BT.VtyEvent ev ->
+     BM.continue =<< BWL.handleListEvent ev msgs
+   _ ->
+     BM.continue msgs
+
+displayActivity chan =
+  void $ BM.customMain (V.mkVty V.defaultConfig) (Just chan) uiApp initialState
+  where
+    initialState = BWL.list () Vec.empty 1
+
+parsingMain = newStats >>= readAndReport newParsers
 
 emptyState = stop WinDbgMsgNull mempty
 
@@ -29,16 +125,6 @@ readAndReport parsers stats = do
   let (amendedParsers, parsers') = feedMessage channelMessage parsers
   reportParsers amendedParsers
   readAndReport parsers' $ updateStats channelMessage newStats
-
-data ChannelStats = ChannelStats { stats_in :: Integer,
-                                   stats_out :: Integer,
-                                   last_msg_time :: T.UTCTime}
-data Direction = Inbound | Outbound deriving Eq
-data ChannelMessage = ChannelMessage Direction ByteString deriving Show
-
-instance Show Direction where
-  show Inbound = "in"
-  show Outbound = "out"
 
 newStats = do
   now <- T.getCurrentTime
